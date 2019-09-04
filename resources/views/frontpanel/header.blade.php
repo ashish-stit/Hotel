aac7c932aaa6852d6a65355d6b00d64a686230c9
ashish.stit <ashish.stit@outlook.com>
2019-09-04 10:51:03 +0530

    'asd'

--- resources/views/frontpanel/header.blade.php
+++ resources/views/frontpanel/header.blade.php
@@ -28,7 +28,7 @@
                 <input type="text" placeholder="Job title, keywords or company name" />
                 <button type="submit"><i class="fa fa-search"></i></button>
             </form>
-            <div class="responsivemenu">
+            <div class="mobile-menu">
                 <ul>
                         <li class="menu-item-has-children">
                             <a href="index.html" title="">Home</a>
@@ -55,10 +55,10 @@
         <div class="menu-sec">
             <div class="container">
                 <div class="logo">
-                    <a href="index.html" title=""><img class="hidesticky" src="{{asset('public/images/logo.png')}}" alt="" /><img class="showsticky" src="{{ asset('public/images/logo.png')}}" alt="" /></a>
+                    <a href="index.html" title=""><img class="hidesticky" src="{{asset('public/images/logo.png')}}" alt="" /><img class="showsticky" src="{{ asset('public/images/logo1.png')}}" alt="" /></a>
                 </div><!-- Logo -->
                 <div class="btn-extars">
-                    <a href="#" title="" class="post-job-btn"><i class="fa fa-plus" aria-hidden="true"></i>
+                    <a title="" class="post-job-btn" id="postjob-popup"><i class="fa fa-plus" aria-hidden="true"></i>
 Post Jobs</a>
                     <ul class="account-btns">
                         <li class="signup-popup"><a  title=""><i class="fa fa-key" aria-hidden="true"></i> Sign Up</a></li>
@@ -68,19 +68,16 @@ Post Jobs</a>
                 <nav>
                     <ul>
                         <li class="menu-item-has-children">
-                            <a href="index.html" title="">
-Home <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                            <a href="index.html" title="">Home</a>
                             
                         </li>
                         
                         <li class="menu-item-has-children">
-                            <a href="about.html" title="">
-About us <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                            <a href="about.html" title="">About us </a>
                             
                         </li>
                         <li class="menu-item-has-children">
-                            <a href="faq.html" title="">FAQs <i class="fa fa-angle-down" aria-hidden="true"></i>
-</a>
+                            <a href="faq.html" title="">FAQs </a>
                         
                         </li>
                         
